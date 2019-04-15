4e01c0d25e1e1fa8f3d8d19422b646a35c58e012
Orestes Polyzos <orestes.polyzos@gmail.com>
2019-04-15 16:00:08 +0300

    Adds support for spring-boot-starter-parent 1.5.20.RELEASE through reflection, but officially uses spring-boot-starter-parent 2.0.0.RELASE

---
+++ src/main/java/ore/spring/web/initializr/exception/ResourceRuntimeException.java
@@ -0,0 +1,16 @@
+package ore.spring.web.initializr.exception;
+
+/**
+ * The type Resource runtime exception.
+ */
+public class ResourceRuntimeException extends RuntimeException {
+
+    /**
+     * Instantiates a new Resource runtime exception.
+     *
+     * @param message the message
+     */
+    public ResourceRuntimeException(String message) {
+        super(message);
+    }
+}
--- src/main/java/ore/spring/web/initializr/service/ResourceService.java
+++ src/main/java/ore/spring/web/initializr/service/ResourceService.java
@@ -4,9 +4,14 @@ import ore.spring.web.initializr.domain.ResourcePersistable;
 import ore.spring.web.initializr.exception.DuplicateResourceException;
 import ore.spring.web.initializr.exception.ResourceException;
 import ore.spring.web.initializr.exception.ResourceNotFoundException;
+import ore.spring.web.initializr.exception.ResourceRuntimeException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.repository.CrudRepository;
 
 import java.io.Serializable;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -19,6 +24,8 @@ import java.util.Optional;
  * @param <ID>  the type parameter used for the Id of the ResourcePersistable
  */
 public abstract class ResourceService<R extends ResourcePersistable<ID>, RSF, ID extends Serializable> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ResourceService.class.getName());
 
     /**
      * The CrudRepository to be used for the ResourcePersistable
@@ -35,23 +42,37 @@ public abstract class ResourceService<R extends ResourcePersistable<ID>, RSF, ID
     }
 
     /**
-     * Finds a ResourcePersistable based on the resourceId
+     * Finds a ResourcePersistable based on the resourceId (will be null if it was not found)
      *
      * @param resourceId the resourceId
      * @return the resourcePersistable that was found
      */
     public R find(ID resourceId) {
-        return crudRepository.findOne(resourceId);
+        return findOptional(resourceId).orElse(null);
     }
 
     /**
      * Find an optional of a ResourcePersistable based on the resourceId
+     * Catching NoSuchMethodError to maintain compatibility with spring-boot-starter-parent less than 2.0.0-RELEASE
      *
      * @param resourceId the resourceId
      * @return the optional of a ResourcePersistable
      */
+    @SuppressWarnings("unchecked")
     public Optional<R> findOptional(ID resourceId) {
-        return Optional.ofNullable(crudRepository.findOne(resourceId));
+        try {
+            return crudRepository.findById(resourceId);
+        } catch (NoSuchMethodError e) {
+            LOGGER.warn("Caught (expected) NoSuchMethodError. Most probably you are using a spring-boot-starter-parent version lower than 2.0.0.RELEASE. It is strongly advised to override this method and use findOne() instead or update your spring-boot-starter-parent-version to (at least) 2.0.0.RELEASE.");
+            try {
+                // Fallback for spring-boot-starter-parent version less than 2.0.0-RELEASE
+                Method findOneMethod = crudRepository.getClass().getMethod("findOne", Serializable.class);
+                return Optional.ofNullable((R) findOneMethod.invoke(crudRepository, resourceId));
+            } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException ex) {
+                LOGGER.error(e.getMessage());
+                throw new ResourceRuntimeException("Caught unexpected NoSuchMethodException during reflection. It is strongly advised to consult the readme file at the Github Repository.");
+            }
+        }
     }
 
     /**
